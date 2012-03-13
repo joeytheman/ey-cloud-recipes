caea524c4c47fa2d47c3b45c57f25a0f265f4d28
Joey <joey@Joeys-Mac-Pro.local>
2012-03-13 12:12:33 -0700

    enable postgis_adapter recipe

--- cookbooks/main/recipes/default.rb
+++ cookbooks/main/recipes/default.rb
@@ -4,6 +4,8 @@
 #  }
 #end
 
+require_recipe "postgis_adapter"
+
 # uncomment to turn on thinking sphinx/ultra sphinx. Remember to edit cookbooks/sphinx/recipes/default.rb first!
  require_recipe "sphinx"
 
